f1013588c34ba76e46e7e75c4fc9467e4632cec6
codybense <codybense@gmail.com>
2024-04-13 07:28:38 -0400

    i hate this thing

--- nixos/sddm-theme.nix
+++ nixos/sddm-theme.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 let
-    imgLink = "https://github.com/CodyBense/wallpapers/gruvbox-mountain-village.png";
+    imgLink = "https://github.com/CodyBense/wallpapers/blob/main/gruvbox-mountain-village.png";
 
     image = pkgs.fetchurl {
         url = imgLink;
