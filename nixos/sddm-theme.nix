c8902cbed00b1f46cfee67b720e216fa113d3bd0
codybense <codybense@gmail.com>
2024-04-13 07:50:48 -0400

    fixing syntax errors

--- nixos/sddm-theme.nix
+++ nixos/sddm-theme.nix
@@ -17,10 +17,10 @@ pkgs.stdenv.mkDerivation {
         sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
     };
     installPhase = ''
-     mkdir -p $out
-     cp -R ./* $out/
-     cd $out/
-     rm Background.jpg
-     cp -r ${image} $out/Background.jpg
+    mkdir -p $out
+    cp -R ./* $out/
+    cd $out/
+    rm Background.jpg
+    cp -r ${image} $out/Background.jpg
     '';
 }
