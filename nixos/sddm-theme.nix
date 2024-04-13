5dbba596e38d854f603a65f70e090e9697a4a7e4
codybense <codybense@gmail.com>
2024-04-13 10:39:38 -0400

    hopefully this helps

--- nixos/sddm-theme.nix
+++ nixos/sddm-theme.nix
@@ -5,7 +5,7 @@ let
 
     image = pkgs.fetchurl {
         url = imgLink;
-        sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0="";
+        sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
     };
 in
 pkgs.stdenv.mkDerivation {
@@ -14,7 +14,7 @@ pkgs.stdenv.mkDerivation {
         owner = "MarianArlt";
         repo = "sddm-sugar-dark";
         rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-        sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+        sha256 = "sha256-SVorwMlHJlQkgxGtNMAGr//ozT6rt6gQ5RFA2cy3nZI=";
     };
     installPhase = ''
     mkdir -p $out
