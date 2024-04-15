5799a8008347440c7e1406f5f4215b1e5a9470b9
codybense <codybense@gmail.com>
2024-04-14 21:44:04 -0400

    added kitty config to home-manager

--- nixos/home.nix
+++ nixos/home.nix
@@ -77,6 +77,23 @@ in
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.kitty = {
+      enable = true;
+      font = {
+          name = "JetBrains Mono";
+          package = pkgs.jetbrains-mono;
+          size = 9;
+      };
+      theme = "Gruvbox Material Dark Hard";
+      settings = {
+          window_padding_width = 25;
+          enable_audio_bell = false;
+          bold_font = auto;
+          italic_font = auto;
+          bold_italic_font = auto;
+      };
+  };
+
   programs.git = {
  	enable = true;
 	userName = "codybense";
