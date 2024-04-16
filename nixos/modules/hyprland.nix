8e919af056d6cc374997d9b45399a6895e5b6b19
codybense <codybense@gmail.com>
2024-04-15 21:45:30 -0400

    added hyprland.nix to modules

---
+++ nixos/modules/hyprland.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ...}:
+{
+    programs.hyprland = {
+        enable = true;
+        #nvidiaPatches = true;
+        xwayland.enable = true;
+        package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    };
+
+    environment.sessionVariables = {
+        #WLR_NO_HARDWARE_CURSORS = "1";
+        NIXOS_OZONE_WL = "1";
+    };
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+}
