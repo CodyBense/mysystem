f40dc61da48e8ccaf3354eea6c94cc15f7994e31
codybense <codybense@gmail.com>
2024-04-16 21:54:27 -0400

    i forgot to uncomment hyprland section in configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -228,19 +228,19 @@
 #	package = config.boot.kernelPackages.nvidiaPackages.stable;
 #  };
 
-  # Hyprland
-  # programs.hyprland = {
-  #  enable = true;
-  #  #nvidiaPatches = true;
-  #  xwayland.enable = true;
-  #  package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  # };
-  #
-  # environment.sessionVariables = {
-  # # WLR_NO_HARDWARE_CURSORS = "1";
-  #  NIXOS_OZONE_WL = "1";
-  # };
-  #
-  # xdg.portal.enable = true;
-  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  Hyprland
+  programs.hyprland = {
+   enable = true;
+   #nvidiaPatches = true;
+   xwayland.enable = true;
+   package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+  };
+
+  environment.sessionVariables = {
+  # WLR_NO_HARDWARE_CURSORS = "1";
+   NIXOS_OZONE_WL = "1";
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
