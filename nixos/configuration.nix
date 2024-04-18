8b1479c4a7b5491bf140a2c7946a9b73cacbdf38
codybense <codybense@gmail.com>
2024-04-17 20:59:35 -0400

    added variable to configuration.nix to set hostName and pull correct hardware-configuration.nix and set it to the system hostName

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -32,7 +32,7 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = ${hostName};
+  networking.hostName = "${hostName}";
   # networking.hostName = "cody-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
