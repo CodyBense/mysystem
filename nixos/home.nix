ddd0dda290935c15dd22370ead8cf2096df5a68f
CodyBense <codybense@gmail.com>
2024-04-18 19:17:15 -0400

    undoing last commit

--- nixos/home.nix
+++ nixos/home.nix
@@ -486,8 +486,8 @@ in
     gtk.cursorTheme.package = pkgs.bibata-cursors;
     gtk.cursorTheme.name = "Bibata-Modern-Ice";
 
-    # gtk.theme.package = pkgs.adw-gtk3;
-    # gtk.theme.name = "adw-gtk3";
+    gtk.theme.package = pkgs.adw-gtk3;
+    gtk.theme.name = "adw-gtk3";
 
     gtk.iconTheme.package = gruvboxPlus;
     gtk.iconTheme.name = "GruvboxPlus";
