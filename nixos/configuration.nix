c7d7efadfbcce6e3d51e7195b5e4f979fef9a967
CodyBense <codybense@gmail.com>
2024-04-23 20:32:10 -0400

    synatx errors

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -108,7 +108,7 @@ if (hostName == "desktop")
     environment.systemPackages = with pkgs; [
 # programs
         neovim
-            vimplugin-nvim-lspconfig
+            nvim-lspconfig
             git
             home-manager
             firefox
