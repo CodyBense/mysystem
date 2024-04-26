bf54b61ac8a029db4a64c229f210f03c6866d41c
CodyBense <codybense@gmail.com>
2024-04-26 16:35:09 -0400

    added back swaylock to home-manager

--- nixos/home.nix
+++ nixos/home.nix
@@ -523,50 +523,30 @@ exec = gsettings set org.gnome.desktop.interface cursor-size 20
 # };
 
 ## swaylock
-# programs.swaylock = {
-#     enable = true;
-#     settings = {
-#         daemonize=true;
-#         show-failed-attempts=true;
-#         clock=true ;
-#         # screenshot;
-#         image = "~/mysystem/wallpapers/latios_latias.jpg";
-#         effect-blur="15x15";
-#         effect-vignette="1:1";
-#         color="1f1d2e80";
-#         font="JetBrainsMono Nerd Font";
-#         indicator=true;
-#         indicator-radius=200;
-#         indicator-thickness=20;
-#         line-color="1f1d2e";
-#         ring-color="191724";
-#         inside-color="1f1d2e";
-#         key-hl-color="eb6f92";
-#         separator-color="00000000";
-#         text-color="e0def4";
-#         text-caps-lock-color="";
-#         line-ver-color="eb6f92";
-#         ring-ver-color="eb6f92";
-#         inside-ver-color="1f1d2e";
-#         text-ver-color="e0def4";
-#         ring-wrong-color="31748f";
-#         text-wrong-color="31748f";
-#         inside-wrong-color="1f1d2e";
-#         inside-clear-color="1f1d2e";
-#         text-clear-color="e0def4";
-#         ring-clear-color="9ccfd8";
-#         line-clear-color="1f1d2e";
-#         line-wrong-color="1f1d2e";
-#         bs-hl-color="31748f";
-#         grace=2;
-#         grace-no-mouse=true;
-#         grace-no-touch=true;
-#         datestr="%a, %B %e";
-#         timestr="%I:%M %p";
-#         fade-in=0.3;
-#         ignore-empty-password=true;
-#     };
-# };
+programs.swaylock = {
+    enable = true;
+    settings = {
+        indicator;
+        ignore-empty-password;
+        indicator-thickness=15;
+        indicator-radius=150;
+        image=~/mysystem/wallpaper/latios_latias.jpg;
+        clock;
+        effect-blur=7x5;
+        effect-vignettee=0.5:0.5;
+        ring-color=98971a;
+        key-hl-color=cc241d;
+        line-color=00000000;
+        inside-color=00000088;
+        text-color=d79921;
+        text-clear-color=daa520;
+        ring-clear-color=890051;
+        separator-color=00000000;
+        grace=0;
+        fade-in=0.5;
+        font=Caskaydia Cove Nerd Font Mono;
+    };
+};
 
 programs.firefox = {
     enable = true;
