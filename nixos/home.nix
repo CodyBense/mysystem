b51da4d5c019e8611053fbe640cb2fc495d111f0
CodyBense <codybense@gmail.com>
2024-04-26 07:16:00 -0400

    fixing error

--- nixos/home.nix
+++ nixos/home.nix
@@ -186,7 +186,7 @@ in
 
 # █░░ ▄▀█ █░█ █▄░█ █▀▀ █░█
 # █▄▄ █▀█ █▄█ █░▀█ █▄▄ █▀█
-        exec-once = bash $scrPath/start.sh
+        exec-once = bash ~mysystem/scripts/start.sh
         
 
 # █▀ █▀█ █░█ █▀█ █▀▀ █▀▀
@@ -433,7 +433,7 @@ bind = $mainMod, A, exec, $menu
 bind = $mainMod, P, pseudo, # dwindle
 bind = $mainMod, F, exec, $browser
 bind = $mainMod, M, exec, spotify
-bind = $mainMod, BACKSPACE, $scrPath/logout_launch.sh
+bind = $mainMod, BACKSPACE, exec, ~/mysystem/scripts/logout_launch.sh
 
 # Move focus with mainMod + vim motions
 bind = $mainMod, H, movefocus, l
