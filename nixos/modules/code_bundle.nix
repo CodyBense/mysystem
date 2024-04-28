5a77c641b8a9232d2048b40eeb44a62b67e4f43d
CodyBense <codybense@gmail.com>
2024-04-27 22:19:46 -0400

    formatting

--- nixos/modules/code_bundle.nix
+++ nixos/modules/code_bundle.nix
@@ -1,5 +1,5 @@
 
-{  lib, config, pkgs, ... }: {
+{  lib, ... }: {
     imports = [
         ./python_module.nix
     ];
--- nixos/modules/python_module.nix
+++ nixos/modules/python_module.nix
@@ -1,20 +1,14 @@
 
 {  lib, config, pkgs, ... }:
-{
-    # environment.systemPackages = with pkgs; [
-    #     python311Packages.pymysql
-    #     python311Packages.mysql-connector
-    #     python311Packages.typer
-    #     python311Packages.inquirer
-    # ];
 
+{
     options = {
         python_module.enable = lib.mkEnableOption "enables python_module";
     };
 
     config = lib.mkIf config.python_module.enable {
         environment.systemPackages = with pkgs; [
-            python3
+            python311Packages.pymysql
             python311Packages.mysql-connector
             python311Packages.typer
             python311Packages.inquirer
