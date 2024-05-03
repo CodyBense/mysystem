0a4e9120770336b9d3cbc8f07e652a258230010a
CodyBense <codybense@gmail.com>
2024-05-03 08:39:55 -0400

    added waybar style to waybar hm module

--- nixos/hm_modules/waybar.nix
+++ nixos/hm_modules/waybar.nix
@@ -103,6 +103,68 @@
             };
         };
 
-        style = 
+        style = ''
+
+            @import "../waybar/colors/gruv-box.css";
+
+            * {
+                border: none;
+                font-family: JetBrainsMono Nerd Font, FontAwesome;
+                font-size: 11px;
+                font-weight: bold;
+                min-height: 0;
+            }
+
+            window#waybar {
+                background-color: @bg;
+                color: @fg1;
+            }
+
+            #clock,
+            #custom-launcher,
+            #clock-date,
+            #workspaces,
+            #pulseaudio,
+            #network,
+            #battery,
+            #workspaces,
+            #custom-powermenu {
+                color: @fg2;
+                background: #433e3c;
+                padding: 0 0.6em;
+                margin-right: 4px; 
+                margin-left: 2px;
+                margin-top: 4px;
+                margin-bottom: 4px;
+                border-radius: 0px;
+            }
+
+
+
+            #workspaces button.active {
+                color: @fg1;
+                background: @bg3;
+            }
+
+            #custom-launcher {
+                background-color: @green;
+                color: @bg;
+                border-bottom-color: #556a35;
+
+                margin-left: 15px;
+                padding-left: 20px;
+                padding-right: 21px;
+            }
+
+            #custom-powermenu {
+                background-color: @red;
+                color: @bg;
+                border-bottom-color: #951c1f;
+
+                margin-right: 15px;
+                padding-left: 20px;
+                padding-right: 23px;
+            }
+        '';
     };
 }
