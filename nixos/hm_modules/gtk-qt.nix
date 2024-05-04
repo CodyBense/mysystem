76cc82f03dc114ef0be39336372d71dd5357b569
CodyBense <codybense@gmail.com>
2024-05-03 21:33:39 -0400

    added gtk/qt hm module and imported it

---
+++ nixos/hm_modules/gtk-qt.nix
@@ -0,0 +1,34 @@
+{ config, inputs, pkgs, ... }:
+
+{
+# enable gtk
+    gtk.enable = true;
+
+    gtk.cursorTheme.package = pkgs.bibata-cursors;
+    gtk.cursorTheme.name = "Bibata-Modern-Ice";
+
+    # gtk.theme.package = pkgs.gruvbox-gtk-theme;
+    # gtk.theme.name = "gruvbox-gtk-theme";
+    
+    gtk.theme.package = pkgs.adw-gtk3;
+    gtk.theme.name = "adw-gtk3";
+
+    # gtk.iconTheme.package = gruvboxPlus;
+    # gtk.iconTheme.name = "GruvboxPlus";
+
+    gtk.iconTheme.package = pkgs.gruvbox-dark-icons-gtk;
+    gtk.iconTheme.name = "gruvbox-dark-icons-gtk";
+
+# enable qt
+    qt.enable = true;
+
+# platform theme "gtk" or "gnome"
+    qt.platformTheme = "qtct";
+    # qt.platformTheme = "kde";
+
+# name of the qt theme
+    qt.style.name = "adwaita-dark";
+
+# packages to use
+    qt.style.package = pkgs.adwaita-qt;
+}
