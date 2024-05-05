ef41a5f4cd9c5e7a4b2c7c9d59233cca339a5518
CodyBense <codybense@gmail.com>
2024-05-05 10:57:39 -0400

    removed python packages

--- nixos/modules/python_module.nix
+++ nixos/modules/python_module.nix
@@ -8,13 +8,13 @@
     config = lib.mkIf config.python_module.enable {
         environment.systemPackages = with pkgs; [
             python3
-            python311Packages.dbus-python
-            python311Packages.pygobject3
             ## List of python packages to pull from
             # python311Packages.pymysql
             # python311Packages.mysql-connector
             # python311Packages.typer
             # python311Packages.inquirer
+            # python311Packages.dbus-python
+            # python311Packages.pygobject3
         ];
     };
 }
