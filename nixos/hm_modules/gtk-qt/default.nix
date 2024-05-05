21da1615eda6d985eedee4f62523f90d954654fc
CodyBense <codybense@gmail.com>
2024-05-05 19:17:42 -0400

    gtk theme

--- nixos/hm_modules/gtk-qt/default.nix
+++ nixos/hm_modules/gtk-qt/default.nix
@@ -115,7 +115,8 @@ in {
   # qt.platformTheme = "kde";
 
 # name of the qt theme
-  qt.style.name = "adwaita-dark";
+  # qt.style.name = "adwaita-dark";
+  qt.style.name = "gruvbox-gtk-theme";
 
 # packages to use
   # qt.style.package = pkgs.adwaita-qt;
