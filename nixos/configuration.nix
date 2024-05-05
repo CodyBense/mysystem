bd795581d3fc2a3e1d175a193662c72ea4aec5c6
CodyBense <codybense@gmail.com>
2024-05-05 17:21:17 -0400

    moved packages to a module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,6 +19,7 @@ in
             inputs.home-manager.nixosModules.home-manager
             ./modules/de_bundle.nix
             ./modules/code_bundle.nix
+            ./modules/packages.nix
             ./../scripts/scripts.nix
         ];
 
@@ -120,100 +121,100 @@ programs.npm.enable = true;
 
 # List packages installed in system profile. To search, run:
 # $ nix search wget
-    environment.systemPackages = with pkgs; [
-# programs
-        neovim
-            vimPlugins.nvim-lspconfig
-            lua54Packages.luasnip
-            home-manager
-            firefox
-            networkmanagerapplet
-            killall
-            fzf
-            zoxide
-            oh-my-posh
-            spotify
-            spicetify-cli
-            curl
-            brightnessctl
-            direnv
-            wlogout
-            unzip
-            nh
-            yad
-            pywal
-            eww
-
-## Terminal stuff
-            tmux
-            gh
-
-## PDF
-# xpdf
-            sioyek # viewer
-
-## IMG
-            swayimg # from terminal using swayimg command
-            feh
-
-## Video
-            vlc
-
-# fonts, icons, and cursors
-            nerdfonts
-            font-awesome
-            jetbrains-mono
-            cascadia-code
-            maple-mono-NF
-            mononoki
-            noto-fonts-cjk-sans
-            material-design-icons
-            bibata-cursors
-# bibata-extra-cursors
-
-# File manager
-            dolphin
-            lf
-            xfce.thunar
-
-# Hyprland stuff
-## bar
-            waybar
-## notifiations
-            dunst
-            libnotify
-## wallpaper
-            swww
-## terminal
-            kitty
-## app launcher
-            rofi-wayland
-## login screen
-            sddm
-## Swaylock
-            swaylock-effects
-            hyprlock
-## Wlogout
-            wlogout
-
-# GTK
-            gtk2
-            gtk3
-            gtk4
-
-# QT
-            qt5ct
-            # qt6ct
-            gsettings-qt
-
-
-## needed for sddm theme
-            libsForQt5.qt5.qtquickcontrols   
-            libsForQt5.qt5.qtquickcontrols2   
-            libsForQt5.qt5.qtgraphicaleffects
-##Code
-            nodejs_21
-            ];
+#     environment.systemPackages = with pkgs; [
+# # programs
+#         neovim
+#             vimPlugins.nvim-lspconfig
+#             lua54Packages.luasnip
+#             home-manager
+#             firefox
+#             networkmanagerapplet
+#             killall
+#             fzf
+#             zoxide
+#             oh-my-posh
+#             spotify
+#             spicetify-cli
+#             curl
+#             brightnessctl
+#             direnv
+#             wlogout
+#             unzip
+#             nh
+#             yad
+#             pywal
+#             eww
+#
+# ## Terminal stuff
+#             tmux
+#             gh
+#
+# ## PDF
+# # xpdf
+#             sioyek # viewer
+#
+# ## IMG
+#             swayimg # from terminal using swayimg command
+#             feh
+#
+# ## Video
+#             vlc
+#
+# # fonts, icons, and cursors
+#             nerdfonts
+#             font-awesome
+#             jetbrains-mono
+#             cascadia-code
+#             maple-mono-NF
+#             mononoki
+#             noto-fonts-cjk-sans
+#             material-design-icons
+#             bibata-cursors
+# # bibata-extra-cursors
+#
+# # File manager
+#             dolphin
+#             lf
+#             xfce.thunar
+#
+# # Hyprland stuff
+# ## bar
+#             waybar
+# ## notifiations
+#             dunst
+#             libnotify
+# ## wallpaper
+#             swww
+# ## terminal
+#             kitty
+# ## app launcher
+#             rofi-wayland
+# ## login screen
+#             sddm
+# ## Swaylock
+#             swaylock-effects
+#             hyprlock
+# ## Wlogout
+#             wlogout
+#
+# # GTK
+#             gtk2
+#             gtk3
+#             gtk4
+#
+# # QT
+#             qt5ct
+#             # qt6ct
+#             gsettings-qt
+#
+#
+# ## needed for sddm theme
+#             libsForQt5.qt5.qtquickcontrols   
+#             libsForQt5.qt5.qtquickcontrols2   
+#             libsForQt5.qt5.qtgraphicaleffects
+# ##Code
+#             nodejs_21
+#             ];
 
 # Fonts
     fonts.packages = with pkgs; [
---
+++ nixos/modules/packages.nix
@@ -0,0 +1,99 @@
+{ pkgs }:
+
+{
+
+    environment.systemPackages = with pkgs; [
+# programs
+        neovim
+        vimPlugins.nvim-lspconfig
+        lua54Packages.luasnip
+        home-manager
+        firefox
+        networkmanagerapplet
+        killall
+        fzf
+        zoxide
+        oh-my-posh
+        spotify
+        spicetify-cli
+        curl
+        brightnessctl
+        direnv
+        wlogout
+        unzip
+        nh
+        yad
+        pywal
+        eww
+
+## Terminal stuff
+        tmux
+        gh
+
+## PDF
+# xpdf
+        sioyek # viewer
+
+## IMG
+        swayimg # from terminal using swayimg command
+        feh
+
+## Video
+        vlc
+
+# fonts, icons, and cursors
+        nerdfonts
+        font-awesome
+        jetbrains-mono
+        cascadia-code
+        maple-mono-NF
+        mononoki
+        noto-fonts-cjk-sans
+        material-design-icons
+        bibata-cursors
+# bibata-extra-cursors
+
+# File manager
+        dolphin
+        lf
+        xfce.thunar
+
+# Hyprland stuff
+## bar
+        waybar
+## notifiations
+        dunst
+        libnotify
+## wallpaper
+        swww
+## terminal
+        kitty
+## app launcher
+        rofi-wayland
+## login screen
+        sddm
+## Swaylock
+        swaylock-effects
+        hyprlock
+## Wlogout
+        wlogout
+
+# GTK
+        gtk2
+        gtk3
+        gtk4
+
+# QT
+        qt5ct
+        qt6ct
+        gsettings-qt
+
+
+## needed for sddm theme
+        libsForQt5.qt5.qtquickcontrols   
+        libsForQt5.qt5.qtquickcontrols2   
+        libsForQt5.qt5.qtgraphicaleffects
+##Code
+        nodejs_21
+            ];
+}
