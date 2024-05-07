3df030c3bce66b3ad399db26a2bcfaa10f4a8ec4
CodyBense <codybense@gmail.com>
2024-05-07 17:47:07 -0400

    fixed hyprland bind, forgot $

--- nixos/hm_modules/hyprland.nix
+++ nixos/hm_modules/hyprland.nix
@@ -291,7 +291,7 @@
                 ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
                 ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
 
-                "$mainMod SHIFT, W, exec, changeWallpaperh"
+                "$mainMod SHIFT, W, exec, $changeWallpaperh"
             ];
 
             bindm = [
