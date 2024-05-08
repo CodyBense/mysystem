d69466d257e5244460d81dc6f48a397f235e8382
CodyBense <codybense@gmail.com>
2024-05-07 21:59:46 -0400

    new wallpaper-switcher script

--- scripts/wallpaper-switcher.nix
+++ scripts/wallpaper-switcher.nix
@@ -93,39 +93,56 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "wallpaper-switcher" ''
-wall_dir="''${HOME}/mysystem/wallpapers"
-cacheDir="''${HOME}/.cache/jp/''${theme}"
-rofi_command="rofi -x11 -dmenu -theme ''${HOME}/.config/rofi/wallSelect.rasi -theme-str ''${rofi_override}"
+wallpaperDir=$HOME/mysystem/wallpapers
+TIMEOUT=720
+  TRANSITION1="--transition-type wave --transition-angle 120 --transition-step 30"
+  TRANSITION2="--transition-type wipe --transition-angle 30 --transition-step 30"
+  TRANSITION3="--transition-type center --transition-step 30"
+  TRANSITION4="--transition-type outer --transition-pos 0.3,0.8 --transition-step 30"
+  TRANSITION5="--transition-type wipe --transition-angle 270 --transition-step 30"
+  WALLPAPER=$(find ''${wallpaperDir} -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
+  PREVIOUS=$WALLPAPER
+  if [ -d ''${wallpaperDir} ]; then
+    num_files=$(ls -1 ''${wallpaperDir} | wc -l)
 
-# Create cache dir if not exists
-if [ ! -d "''${cacheDir}" ] ; then
-        mkdir -p "''${cacheDir}"
+    if [ $num_files -lt 1 ]; then
+      notify-send -t 9000 "The wallpaper folder is expected to have more than 1 image. Exiting Wallsetter."
+      exit
+    else
+      cd ''${wallpaperDir}
+      if [ -d ".git" ]; then
+        git pull
+      else
+        notify-send -t 9000 "The wallpaper directory is expected to be a Git repository. Exiting Wallsetter."
+        exit
+      fi
     fi
-
-
-physical_monitor_size=13
-monitor_res=$(hyprctl monitors |grep -A2 Monitor |head -n 2 |awk '{print $1}' | grep -oE '^[0-9]+')
-dotsperinch=$(echo "scale=2; $monitor_res / $physical_monitor_size" | bc | xargs printf "%.0f")
-monitor_res=$(( $monitor_res * $physical_monitor_size / $dotsperinch ))
-
-rofi_override="element-icon{size:''${monitor_res}px;border-radius:0px;}"
-
-# Convert images in directory and save to cache dir
-for imagen in "$wall_dir"/*.{jpg,jpeg,png,webp}; do
-	if [ -f "$imagen" ]; then
-		nombre_archivo=$(basename "$imagen")
-			if [ ! -f "''${cacheDir}/''${nombre_archivo}" ] ; then
-				convert -strip "$imagen" -thumbnail 500x500^ -gravity center -extent 500x500 "''${cacheDir}/''${nombre_archivo}"
-			fi
+  while true;
+  do
+    if [ "$WALLPAPER" == "$PREVIOUS" ]; then
+      WALLPAPER=$(find ''${wallpaperDir} -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
+    else
+      PREVIOUS=$WALLPAPER
+      NUM=$(shuf -e 1 2 3 4 5 -n 1)
+      case $NUM in
+        1)
+          TRANSITION=$TRANSITION1
+        ;;
+        2)
+          TRANSITION=$TRANSITION2
+        ;;
+        3)
+          TRANSITION=$TRANSITION3
+        ;;
+        4)
+          TRANSITION=$TRANSITION4
+        ;;
+        5)
+          TRANSITION=$TRANSITION5
+        ;;
+      esac
+      ''${pkgs.swww}/bin/swww img "$WALLPAPER" $TRANSITION
+      sleep $TIMEOUT
     fi
-done
-
-# Select a picture with rofi
-wall_selection=$(find "''${wall_dir}"  -maxdepth 1  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n" ; done | $rofi_command)
-
-# Set the wallpaper
-[[ -n "$wall_selection" ]] || exit 1
-swww img ''${wall_dir}/''${wall_selection}
-
-exit 0
+  done
 ''
