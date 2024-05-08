efefd679055134d87655232cf3edd7b8dfd51cdc
CodyBense <codybense@gmail.com>
2024-05-08 17:50:46 -0400

    finished wallpaper-switcher

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,17 +2,9 @@
 
 DIR=$HOME/mysystem/wallpapers
 arr=($DIR/*)
-#
-# for ((i=0; i<${#arr[@]}; i++)); do
-#     echo "${arr[$i]}"
-# done
-
-IFS=$'\n'
-files="$(fd --search-path '$HOME/mysystem/wallpapers')"
 filenames=()
 for e in ${arr[@]}; do
     filenames=("${filenames[@]}" ${e##*/}) 
 done
 choice=$(printf "%s\n" "${filenames[@]}" | rofi -dmenu) 
-swww -"img" ${choice}
-echo $choice_path
+swww img ${DIR}/${choice}
--- scripts/wallpaper-switcher.nix
+++ scripts/wallpaper-switcher.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "wallpaper-switcher" ''
-DIR=$HOME/mysystem/wallpapers/
-arr=(''${DIR}/*)
-
+DIR=$HOME/mysystem/wallpapers
+arr=($DIR/*)
+filenames=()
+for e in ''${arr[@]}; do
+    filenames=("''${filenames[@]}" ''${e##*/}) 
+done
+choice=$(printf "%s\n" "''${filenames[@]}" | rofi -dmenu) 
+swww img ''${DIR}/''${choice}
 ''
