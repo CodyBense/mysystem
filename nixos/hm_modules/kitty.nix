b5f4ef395a8eee4bcedeb5024a2f6321432bd6b2
CodyBense <codybense@gmail.com>
2024-05-09 23:36:03 -0400

    added more colors to kitty

--- nixos/hm_modules/kitty.nix
+++ nixos/hm_modules/kitty.nix
@@ -14,7 +14,35 @@
             bold_font = "auto";
             italic_font = "auto";
             bold_italic_font = "auto";
+            background_opacity = "0.7";
             background = "#${base00}";
+            foreground = "#${base07}";
+            selection_background = "#${base03}";
+            selction_foreground = "#${base04}";
+            #black
+            color0 = "#${base01}";
+            color8 = "#${base01}";
+            #red
+            color1 = "#${base08}";
+            color9 = "#${base08}";
+            #green
+            color2 = "#${base0B}";
+            color10 = "#${base0B}";
+            #yellow
+            color3 = "#${base0A}";
+            color11 = "#${base0A}";
+            #blue
+            color4 = "#${base0D}";
+            color12 = "#${base0D}";
+            #magenta
+            color5 = "#${base0E}";
+            color13 = "#${base0E}";
+            #cyan
+            color6 = "#${base0C}";
+            color14 = "#${base0C}";
+            #white
+            color7 = "#${base05}";
+            color15 = "#${base05}";
         };
         shellIntegration.enableBashIntegration = true;
     };
