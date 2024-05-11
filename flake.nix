df765805ba6d3759c5a97eedfe4e83e91d80f39a
CodyBense <codybense@gmail.com>
2024-05-11 08:19:12 -0400

    got it back to working

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
             modules = [
                 hyprland.homeManagerModules.default
                 {wayland.windowManager.hyprland.enable = true;}
+                ./nixos/hm_modules/spicetify.nix
             ];
         };
 
