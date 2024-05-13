1c65bd96107dde0e77a6b9721ea8a2bb48a9d85c
CodyBense <codybense@gmail.com>
2024-05-13 19:11:50 -0400

    zshrc stuff

--- nixos/hm_modules/zshrc.nix
+++ nixos/hm_modules/zshrc.nix
@@ -2,6 +2,7 @@
 {
     programs.zsh = {
         enable = true;
+        dotDir = "~/.zshrc";
         enableCompletion = true;
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
