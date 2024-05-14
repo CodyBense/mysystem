267a6566098af7fcb96f59aa21de80ca0a3f625e
CodyBense <codybense@gmail.com>
2024-05-13 22:00:09 -0400

    added random shiny to randomPokemon.nix

--- scripts/randomPokemon.nix
+++ scripts/randomPokemon.nix
@@ -4,7 +4,14 @@ pkgs.writeShellScriptBin "randomPokemon" ''
 pokemon=("gengar" "latios" "charizard" "typhlosion" "emboar" "hydreigon" "garchomp" "infernape" "salamence" "flygon" "mudkip" "scizor" "dragonite" "snorlax")
 
 size=''${#pokemon[@]}
-index=''$(($RANDOM % $size))
+index=''$((''$RANDOM % ''$size))
 
-pokeget --hide-name ''${pokemon[$index]}
+shiny=''$((''$RANDOM%2))
+
+if [ ''$shiny -eq 0 ]
+then
+    pokeget --hide-name ''${pokemon[$index]}
+else
+    pokeget --hide-name ''${pokemon[$index]} -s
+fi
 ''
