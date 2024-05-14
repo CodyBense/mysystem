bdcf3de8d6d8470d24869ffa397fde20357c44ae
CodyBense <codybense@gmail.com>
2024-05-13 21:51:33 -0400

    added more pokemon

--- scripts/randomPokemon.nix
+++ scripts/randomPokemon.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "randomPokemon" ''
-pokemon=("gengar" "latios" "charizard" "typhlosion")
+pokemon=("gengar" "latios" "charizard" "typhlosion" "emboar" "hydreigon" "garchomp" "infernape" "salamence" "flygon" "mudkip" "scizor" "dragonite" "snorlax")
 
 size=''${#pokemon[@]}
 index=''$(($RANDOM % $size))
