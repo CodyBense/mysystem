1dafe51fadcb3b412a50cb7ac2cc75383def1366
CodyBense <codybense@gmail.com>
2024-05-17 20:52:41 -0400

    added to lf hm module

--- nixos/hm_modules/lf.nix
+++ nixos/hm_modules/lf.nix
@@ -2,5 +2,11 @@
 {
     programs.lf = {
         enable = true;
+        settings = {
+            hidden = true;
+            ignorecase = true;
+            icon = true;
+            sixel = true;
+        };
     };
 }
