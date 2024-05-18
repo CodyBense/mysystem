1e5d1bb28d9f4fa34b29964ba2137e0b30035e80
CodyBense <codybense@gmail.com>
2024-05-17 22:39:09 -0400

    hm module lf, testing

--- nixos/hm_modules/lf.nix
+++ nixos/hm_modules/lf.nix
@@ -10,37 +10,37 @@
             rulerfmt = "%d  |%a  |%p  |\033[7;31m %m \033[0m  |\033[7;33m %c \033[0m  |\033[7;35m %s \033[0m  |\033[7;34m %f \033[0m  |%i/%t";
         };
         commands = {
-            mkdir = "\${{
+            mkdir = "
+                     \${{
                      printf \"Directory name: \"
                      read ans
-                     mkdir $ans}}";
-            mkfile = "\${{
+                     mkdir $ans}}
+                     ";
+            mkfile = "
+                     \${{
                      printf \"File name: \"
                      read ans
-                     $EDITOR $ans}}";
-            unarchive = "\${{
-                        case \"$f\" in
-                            *.zip) unzip \"$f\" ;;
-                            *.rar) unrar x \"$f\" ;;
-                            *.tar.gz) tar -xzvf \"$f\" ;;
-                            *.tar.bz2) tar -xjvf \"$f\" ;;
-                            *.tar) tar -xvf \"$f\" ;;
-                            *.7z) 7z e \"$f\" ;;
+                     $EDITOR $ans}}
+                     ";
+            unarchive = "
+                        \${{
+                        case $f in
+                            *.zip) unzip $f ;;
+                            *.rar) unrar x $f ;;
+                            *.tar.gz) tar -xzvf $f ;;
+                            *.tar.bz2) tar -xjvf $f ;;
+                            *.tar) tar -xvf $f ;;
+                            *.7z) 7z e $f ;;
                         *) echo \"Unsupported format\" ;;
-                        esac}}";
-            trash = "\${{
-                    files=\"$\"(printf \"$\"fx | tr '\n' ';')
-                    while [ \"$\"files ]; do
-                        file="\$\"{files%%;*}
-
-                        trashy put "\$\"(basename "\$\"file)"
-                        if [ "\$\"files = "\$\"file ]; then
-                            files=''
-                        else
-                            files="\$\"{files#*;};
-                        fi
-                    done
-            }}";
+                        esac}}
+                        ";
+            open = "
+                   &{{
+                       case $(file --mime-type -Lb $f) in
+                        text/*) lf -remote \"send $id \$nvim \$fx\";;
+                        *) xdg-open \"$f\"
+                        esac
+                   }}";
         };
         keybindings = {
             # c = null;
