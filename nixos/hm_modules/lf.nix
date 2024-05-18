f9aec46b0d51ef7da8188b36650b932ed84b92b7
CodyBense <codybense@gmail.com>
2024-05-17 21:05:14 -0400

    hm module lf, testing cmd

--- nixos/hm_modules/lf.nix
+++ nixos/hm_modules/lf.nix
@@ -7,6 +7,10 @@
             ignorecase = true;
             icons = true;
             sixel = true;
+            rulerfmt = "%d  |%a  |%p  |\033[7;31m %m \033[0m  |\033[7;33m %c \033[0m  |\033[7;35m %s \033[0m  |\033[7;34m %f \033[0m  |%i/%t";
+        };
+        commands = {
+            mkdir = "printf \"Directory name: ""read ans mkdir $ans";
         };
     };
 }
