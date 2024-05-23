b95c967844107cf750abc2c480975aa54e5eeb1c
CodyBense <codybense@gmail.com>
2024-05-23 19:20:13 -0400

    tyring to fix stylix

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 
 {
-    stylix.image =  ../../../wallpapers/ign_wifu.png;
+    # stylix.image =  ../../../wallpapers/ign_wifu.png;
+    stylix.image = nil;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     stylix.cursor.package = pkgs.bibata-cursors;
     stylix.cursor.name = "Bibata-Modern-Ice";
