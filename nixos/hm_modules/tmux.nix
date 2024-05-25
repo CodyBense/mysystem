a8ba2d5cffd9133c9c2a018ea22b95d0660bfa42
CodyBense <codybense@gmail.com>
2024-05-24 21:16:46 -0400

    hm module tmux, updating

--- nixos/hm_modules/tmux.nix
+++ nixos/hm_modules/tmux.nix
@@ -2,7 +2,7 @@
 {
     programs.tmux = {
         enable = true;
-        shell = "\${pkgs.zsh}/bin/zsh";
+        shell = "${pkgs.zsh}/bin/zsh";
         baseIndex = 1;
         newSession = true;
     };
