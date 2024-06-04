58d4f3d5495bba3e387b756b04b7e0bc293d024b
CodyBense <codybense@gmail.com>
2024-06-04 18:11:53 -0400

    hm module spicetify, testing

--- nixos/hm_modules/spicetify.nix
+++ nixos/hm_modules/spicetify.nix
@@ -31,9 +31,9 @@ let
 in
 {
   # allow spotify to be installed if you don't have unfree enabled already
-  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-  #   "spotify"
-  # ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "spotify"
+  ];
 
   # import the flake's module for your system
   imports = [ spicetify-nix.homeManagerModule ];
