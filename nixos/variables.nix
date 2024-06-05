585883903415d2b7da0e17be0d0718730ed34aae
CodyBense <codybense@gmail.com>
2024-06-05 17:52:00 -0400

    renamed options.nix to variables.nix, added some stuff to it

---
+++ nixos/variables.nix
@@ -0,0 +1,10 @@
+{
+    hostName = "laptop";
+
+    # Git config
+    gitUsername = "Cody Bense";
+    gitEmail = "codyebense@gmail.com";
+
+    # Base16 theme
+    theme = "gruvbox-dark-medium";
+}
