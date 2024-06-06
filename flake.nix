6d26b31e3fe4e974aa75f123cd434cb4d9899d23
CodyBense <codybense@gmail.com>
2024-06-05 22:09:24 -0400

    redoing

--- flake.nix
+++ flake.nix
@@ -3,63 +3,54 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
-        # hyprland.url = "github:hyprwm/Hyprland";
-        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
         nix-colors.url = "github:misterio77/nix-colors";
-
-        stylix.url = "github:danth/stylix";
-
-        spicetify-nix.url = "github:the-argus/spicetify-nix";
+        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     };
 
-    outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: 
-    let
+    outputs = { nixpkgs, home-manager, ... }@inputs: 
+        let
         system = "x86_64-linux";
+    host = "laptop";
+    username = "codybense";
 
-        pkgs = import nixpkgs {
-            inherit system;
-
-            config = {
-                allowUnfree = true;
-            };
+    pkgs = import nixpkgs {
+        inherit system;
+        config = {
+            allowUnfree = true;
         };
+    };
     in
     {
 
-        homeConfigurations."codybense@laptop" = home-manager.lib.homeManagerConfiguration {
-            # pkgs = nixpkgs.legacyPackages.${system};
-            inherit pkgs;
-
-            extraSpecialArgs = { 
-                inherit inputs;
-            };
-
-            modules = [
-                hyprland.homeManagerModules.default
-                {wayland.windowManager.hyprland.enable = true;}
-                ./nixos/hm_modules/spicetify.nix
-            ];
-        };
-
         nixosConfigurations = {
-            laptop = nixpkgs.lib.nixosSystem {
+            "${host}" = nixpkgs.lib.nixosSystem {
                 specialArgs = { 
-                    inherit inputs;
                     inherit system;
+                    inherit inputs;
+                    inherit username;
+                    inherit host;
                 };
 
                 modules = [
-                    ./nixos/configuration.nix
-                    # inputs.stylix.nixosModules.stylix
+                    ./hosts/${host}/configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.extraSpecailArgs = {
+                                inherit username;
+                                inherit inputs;
+                                inherit host;
+                                inherit (inputs.nix-colors.lib-contrib { inherit pkgs; }) gtkThemeFromScheme;
+                            };
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.backupFileExtension = "backup";
+                            home-manager.users.${username} = import ./hosts/${host}/home.nix;
+                        };
                 ];
             };
         };
-    };
-}
+    }
