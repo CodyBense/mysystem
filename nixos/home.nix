ab4f457b6a04fbff5087dc84df93f7dd2e6f1275
CodyBense <codybense@gmail.com>
2024-06-05 21:23:08 -0400

    testing

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,5 +1,5 @@
 # PROGRAMS to get to dotfile configuration control
-{ config, inputs, pkgs, ... }:
+{ config, inputs, pkgs, username, ... }:
 let
     palette = config.colorScheme.palette;
     inherit (import ./variables.nix)
@@ -31,7 +31,7 @@ in
          ./colors/colors.nix
     ];
 
-    home.username = "codybense";
+    home.username = "${username}";
     home.homeDirectory = "/home/codybense";
 
     home.stateVersion = "23.11"; # Please read the comment before changing.
