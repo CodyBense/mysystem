d8129deb02900e529ea4e3858ddcc4884e2328e7
CodyBense <codybense@gmail.com>
2024-06-07 17:37:23 -0400

    fixing errors

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -60,7 +60,7 @@ in
     # Scripts
     home.packages = [
         (import ../../scripts/randomPokemon.nix {inherit pkgs;})
-        (import ../../scirpts/start-hyprland.nix {inherit pkgs;})
+        (import ../../scripts/start-hyprland.nix {inherit pkgs;})
     ];
 
 # Let Home Manager install and manage itself.
