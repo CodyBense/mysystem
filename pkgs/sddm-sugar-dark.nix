476edd54317f10a3da73804223a6c58ae8f7db3d
CodyBense <codybense@gmail.com>
2024-06-07 21:54:44 -0400

    fixing errors

--- pkgs/sddm-sugar-dark.nix
+++ pkgs/sddm-sugar-dark.nix
@@ -1,25 +1,28 @@
-{ pkgs }:
+{ stdenv, fetchFromGitHub, fetchurl }:
 
 let
-    imgLink = "https://raw.githubusercontent.com/Goxore/dotfiles/50db864d56d49768f1d4d0a8c1bd7a5c74dd629e/home/Wallpapers/gruvbox-mountain-village.png";
+imgLink = "https://raw.githubusercontent.com/Goxore/dotfiles/50db864d56d49768f1d4d0a8c1bd7a5c74dd629e/home/Wallpapers/gruvbox-mountain-village.png";
 
-    image = pkgs.fetchurl {
-        url = imgLink;
-        sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
-    };
+image = fetchurl {
+    url = imgLink;
+    sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
+};
 in
+{
 
-pkgs.stdenv.mkDerivation {
-  pname = "sddm-sugar-dark-theme";
-  version = "1.2";
-  installPhase = ''
-    mkdir -p $out/share/sddm/themes
-    cp -aR $src $out/share/sddm/themes/sugar-dark
-  '';
-  src = pkgs.fetchFromGitHub {
-    owner = "MarianArlt";
-    repo = "sddm-sugar-dark";
-    rev = "v${version}";
-    sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-  };
+    sddm-sugar-dark = stdenv.mkDerivation rec {
+        pname = "sddm-sugar-dark-theme";
+        version = "1.2";
+        dontBuild = true;
+        installPhase = ''
+            mkdir -p $out/share/sddm/themes
+            cp -aR $src $out/share/sddm/themes/sugar-dark
+        '';
+        src = fetchFromGitHub {
+            owner = "MarianArlt";
+            repo = "sddm-sugar-dark";
+            rev = "v${version}";
+            sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+        };
+    };
 }
