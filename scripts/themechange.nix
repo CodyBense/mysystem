c387b0d5b0d64321b9b375ccb837644f0aa98a7e
CodyBense <codybense@gmail.com>
2024-06-07 22:58:13 -0400

    fixing errors

--- scripts/themechange.nix
+++ scripts/themechange.nix
@@ -5,7 +5,7 @@ pkgs.writeShellScriptBin "themechange" ''
     echo "No Theme Given"
   else
     replacement="$1"
-    sed -i "/^\s*theme[[:space:]]*=[[:space:]]*\"/s/\"\(.*\)\"/\"$replacement\"/" /home/${username}/zaneyos/hosts/${host}/variables.nix
-    kitty -e pkexec nixos-rebuild switch --flake /home/${username}/zaneyos
+    sed -i "/^\s*theme[[:space:]]*=[[:space:]]*\"/s/\"\(.*\)\"/\"$replacement\"/" /home/${username}/mysystem/hosts/${host}/variables.nix
+    kitty -e pkexec nixos-rebuild switch --flake /home/${username}/mysystem
   fi
 ''
