7c559a28ba36a417e3f9770f8c8cf5e991c3aae3
CodyBense <codybense@gmail.com>
2024-06-08 22:40:29 -0400

    sddm testing

--- pkgs/sddm-sugar-dark.nix
+++ pkgs/sddm-sugar-dark.nix
@@ -17,6 +17,8 @@ in
         installPhase = ''
             mkdir -p $out/share/sddm/themes
             cp -aR $src $out/share/sddm/themes/sugar-dark
+            cd $out/share/sddm/themes/sugar-dark
+            cp -r ${image} $out/share/sddm/themes/sugar-dark/Background.jpg
         '';
         src = fetchFromGitHub {
             owner = "MarianArlt";
