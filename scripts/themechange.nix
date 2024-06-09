5d9d20da3bdfda4800a14e5c0122fb8d2bb2c410
CodyBense <codybense@gmail.com>
2024-06-09 09:27:32 -0400

    testing

--- scripts/themechange.nix
+++ scripts/themechange.nix
@@ -6,6 +6,7 @@ pkgs.writeShellScriptBin "themechange" ''
   else
     replacement="$1"
     sed -i "/^\s*theme[[:space:]]*=[[:space:]]*\"/s/\"\(.*\)\"/\"$replacement\"/" /home/${username}/mysystem/hosts/${host}/variables.nix
-    kitty -e pkexec nixos-rebuild switch --flake /home/${username}/mysystem
+    # kitty -e pkexec nixos-rebuild switch --flake /home/${username}/mysystem
+    echo "ZSe45rdx##" | sudo -S nixos-rebuild switch --flake /home/${username}/mysystem
   fi
 ''
