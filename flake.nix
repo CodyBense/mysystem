058ed70e4bfbec15df7d72938478caddf4b542cc
CodyBense <codybense@gmail.com>
2024-06-10 18:18:51 -0400

    i hate hyprland plugins, ie hy3

--- flake.nix
+++ flake.nix
@@ -8,8 +8,7 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
         nix-colors.url = "github:misterio77/nix-colors";
-        # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/v0.40.0";
+        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
         hyprland-plugins = {
             url = "github:hyprwm/hyprland-plugins";
             inputs.hyprland.follows = "hyprland";
