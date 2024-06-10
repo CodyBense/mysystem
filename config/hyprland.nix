3303a25507ca3ef8a3fa528cf1a9572fcb57dec6
CodyBense <codybense@gmail.com>
2024-06-10 19:57:08 -0400

    hyprland plugin test

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -13,8 +13,8 @@ with lib;
         systemd.enable = true;
         plugins = [
             #hyprplugins.{pluginname}
-            # hyprplugins.hyprexpo
-            hy3.packages.x86_64-linux.hy3
+            hyprplugins.hyprexpo
+            # hy3.packages.x86_64-linux.hy3
         ];
         # extraConfig = ''
         #     plugin = ${hy3.packages.x86_64-linux.hy3}/lib/libhy3.so
