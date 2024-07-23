3707e32bde50c02b5fffc5453f862f322665a3f9
CodyBense <codybense@gmail.com>
2024-07-23 17:50:10 -0400

    updating nvidia stuff

--- modules/nix/de/nvidia_module.nix
+++ modules/nix/de/nvidia_module.nix
@@ -1,25 +1,25 @@
 { lib, config, pkgs, ...}:
 {
-    options = {
-        nvidia_module.enable = lib.mkEnableOption "enables nvidia_module"; 
-    };
+        options = {
+            nvidia_module.enable = lib.mkEnableOption "enables nvidia_module"; 
+        };
 
-    config = lib.mkIf config.nvidia_module.enable{
-        hardware.opengl = {
-            enable = true;
-            driSupport32Bit = true;
+        config = lib.mkIf config.nvidia_module.enable{
+            hardware.opengl = {
+                enable = true;
+                driSupport32Bit = true;
         };
 
         # Load nvidia driver
         services.xserver.videoDrivers = ["nvidia"];
 
-        #  hardware.nvidia = {
-        #     modesetting.enable = true;
-        #     powerManagement.enable = true;
-        #     powerManagement.finegrained = false;
-        #     open = false;
-        #     nvidiaSettings = true;
-        #     package = config.boot.kernelPackages.stable;
-        # };
+        hardware.nvidia = {
+            modesetting.enable = true;
+            powerManagement.enable = true;
+            powerManagement.finegrained = false;
+            open = false;
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.stable;
+        };
     };
 }
