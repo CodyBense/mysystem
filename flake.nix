cfa9fb98eecdeb3d94c9af3beb575b6edefd674e
CodyBense <codybense@gmail.com>
2024-07-25 22:31:31 -0400

    changed host name for desktop

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     outputs = { nixpkgs, home-manager, hy3, spicetify-nix, ... }@inputs: 
     let
         system = "x86_64-linux";
-        host = "laptop";
+        host = "nixos";
         username = "codybense";
  
         pkgs = import nixpkgs {
