611d231735f1e483ad2b84d9c4d4e05d8ea968dc
CodyBense <codybense@gmail.com>
2024-08-26 21:17:44 -0400

    customPkgs, updated todo_go.nix version and hash

--- pkgs/customPkgs/todo_go.nix
+++ pkgs/customPkgs/todo_go.nix
@@ -1,34 +1,16 @@
-# { pkgs, ... }:
-# {
-#     nixpkgs.overlays = [
-#     (final: prev: {
-#       todo = pkgs.buildGoModule {
-#         pname = "todo_go";
-#         src = pkgs.fetchFromGitHub {
-#           owner = "CodyBense";
-#           repo = "todo_go";
-#           rev = "0eea65f0c2e36bbd538fe939428583d23fa6921b";
-#           sha256 = "10h2crpsfdv3i3jsx64nswnvddap912bnv7rhwfl3pwqysqywrbq";
-#         };
-#         vendorHash = "sha256-eGXusfaY30Edh/lsu0RIV7W2LdeWmK7liGM3p29mAoI=";
-#         nativeCheckInputs = with pkgs; [ less ];
-#       };
-#     })
-#   ];
-# }
 { pkgs, lib, ... }:
 {
     nixpkgs.overlays = [
         (final: prev: {
          todo = pkgs.buildGoModule rec {
          pname = "todo_go";
-         version = "1.4.0";
+         version = "1.4.1";
 
          src = pkgs.fetchFromGitHub {
          owner = "CodyBense";
          repo = "todo_go";
          rev = "v${version}";
-         hash = "sha256-QsS7WtsTt0Y9ZduXlGyIOcp0ECUB2e7WbmMwa28MZnQ=";
+         hash = "sha256-Irh9K2Ui9ZEHhJQeoK55TZHrn1jHtwhDbrnZLSzYI2o=";
          };
 
          vendorHash = null;
